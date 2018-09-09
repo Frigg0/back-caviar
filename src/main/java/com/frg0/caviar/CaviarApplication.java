cb33a612056a8cd0ea38503c364006d7dc3942a2
Frg0 <wojtysiak.t@gmail.com>
2018-09-09 18:35:47 +0200

    Initialization

---
+++ src/main/java/com/frg0/caviar/CaviarApplication.java
@@ -0,0 +1,12 @@
+package com.frg0.caviar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaviarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaviarApplication.class, args);
+    }
+}
